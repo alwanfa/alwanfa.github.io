7f5735a004358b2e46c8a264fb86536580ed2cfd
WanDev <71076451+alwanfa@users.noreply.github.com>
2021-11-09 12:41:28 +0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,37 @@
+function moveToHomePage() {
+    document.getElementById("homepage").style.opacity = 1;
+    document.getElementById("resume").style.opacity = 0;
+    document.getElementById("resume").style.top = "-15%";
+    document.getElementById("project").style.opacity = 0;
+    document.getElementById("project").style.top = "-60%";
+    document.getElementById("contact").style.opacity = 0;
+}
+
+function moveToResume() {
+    document.getElementById("resume").style.opacity = 1;
+    document.getElementById("homepage").style.opacity = 0;
+    document.getElementById("resume").style.top = "15%";
+    document.getElementById("project").style.opacity = 0;
+    document.getElementById("project").style.top = "-60%";
+    document.getElementById("contact").style.opacity = 0;
+}
+
+function moveToProject() {
+    document.getElementById("contact").style.opacity = 0;
+    document.getElementById("project").style.opacity = 1;
+    document.getElementById("project").style.top = "15%"
+    document.getElementById("resume").style.opacity = 0;
+    document.getElementById("resume").style.top = "-15%";
+    document.getElementById("homepage").style.opacity = 0;
+
+}
+
+function moveToContact() {
+    document.getElementById("contact").style.opacity = 1;
+    document.getElementById("project").style.opacity = 0;
+    document.getElementById("project").style.top = "-60%"
+    document.getElementById("resume").style.opacity = 0;
+    document.getElementById("resume").style.top = "-15%";
+    document.getElementById("homepage").style.opacity = 0;
+
+}
