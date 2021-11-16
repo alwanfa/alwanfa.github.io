70ba2d330cb8c5e70a9b0b5b45a7c266dad0e4b7
WanDev <71076451+alwanfa@users.noreply.github.com>
2021-11-16 22:25:12 +0700

    Update home.js

--- home.js
+++ home.js
@@ -1,18 +1,20 @@
 function moveToHomePage() {
     document.getElementById("homepage").style.opacity = 1;
     document.getElementById("resume").style.opacity = 0;
-    document.getElementById("resume").style.top = "-15%";
+    document.getElementById("resume").style.top = "-60%";
     document.getElementById("project").style.opacity = 0;
     document.getElementById("project").style.top = "-60%";
     document.getElementById("contact").style.opacity = 0;
+    document.title = "Home | wanDev";
 }
 
 function moveToResume() {
     document.getElementById("resume").style.opacity = 1;
     document.getElementById("homepage").style.opacity = 0;
     document.getElementById("resume").style.top = "15%";
+    document.getElementById("resume").style.right = "0%";
     document.getElementById("project").style.opacity = 0;
-    document.getElementById("project").style.top = "-60%";
+    document.getElementById("project").style.top = "-80%";
     document.getElementById("contact").style.opacity = 0;
     document.title = "Resume | wanDev";
 }
@@ -32,8 +34,31 @@ function moveToContact() {
     document.getElementById("project").style.opacity = 0;
     document.getElementById("project").style.top = "-60%"
     document.getElementById("resume").style.opacity = 0;
-    document.getElementById("resume").style.top = "-15%";
+    document.getElementById("resume").style.top = "-60%";
     document.getElementById("homepage").style.opacity = 0;
     document.title = "Contact | wanDev";
 
+}
+var condition = "none";
+
+function hamburgerMenu() {
+
+    if (condition != "active") {
+        condition = "active";
+        document.getElementById("child1").style.transform = "rotate(45deg) translate(-4px, -4px)";
+        document.getElementById("child1").style.transformOrigin = 0;
+        document.getElementById("child2").style.transform = "scale(0)";
+        document.getElementById("child3").style.transform = "rotate(-45deg) translate(0px, 0px)";
+        document.getElementById("child3").style.transformOrigin = 0;
+        document.getElementById("list-link").style.right = "0%";
+
+    } else {
+        condition = "none";
+        document.getElementById("child1").style.transform = "rotate(0deg) translate(0px, 0px)";
+        document.getElementById("child1").style.transformOrigin = 0;
+        document.getElementById("child2").style.transform = "scale(1)";
+        document.getElementById("child3").style.transform = "rotate(0deg) translate(0px, 0px)";
+        document.getElementById("child3").style.transformOrigin = 0;
+        document.getElementById("list-link").style.right = "-100%";
+    }
 }
